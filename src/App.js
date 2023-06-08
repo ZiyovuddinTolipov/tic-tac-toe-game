def7896a9b9cdab4d59b71f5bb7d9c7ac4c9a80c
tolipov <ziyovuddin.developer@gmail.com>
2023-06-08 12:33:03 +0500

    add  Square.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 
 import './scss/App.scss';
 import Board from './Components/Board';
+import Square from './Components/Square';
 
 
 function App() {
   return (
     <main>
       <Board>
-
+        <Square />
+        <Square />
+        <Square />
+        {/*  */}
+        <Square />
+        <Square />
+        <Square />
+        {/*  */}
+        <Square />
+        <Square />
+        <Square />
       </Board>
     </main>
   );
